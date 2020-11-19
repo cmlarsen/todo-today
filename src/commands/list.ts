e3e84dc5f61681b3490c317a71931125532e783b
Caleb Larsen <cmlarsen@gmail.com>
2020-11-19 12:28:41 -0500

    Remove unused options and args for `list`

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -20,13 +20,7 @@ export default class List extends Command {
 
   static flags = {
     help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: "f" }),
   };
-
-  static args = [{ name: "file" }];
 
   async run() {
     const { args, flags } = this.parse(List);
