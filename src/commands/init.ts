6904bb355d90b5e3651406776ad49374d1da05a9
Caleb Larsen <cmlarsen@gmail.com>
2020-11-20 17:34:47 -0500

    Improve help on `init`

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -4,7 +4,7 @@ import { Command, flags } from "@oclif/command";
 export default class Init extends Command {
   static description = `Connect to your Todoist account. You can find this in Todoist under Settings -> Integrations -> API token. It will look something like this: 9d95f4419a485ae8ba935b44b202ad38a64eaasd`;
 
-  static examples = [`$ today init [Your Key Here]`];
+  static examples = [`$ today init TOKEN`];
 
   static flags = {
     help: flags.help({ char: "h" }),
